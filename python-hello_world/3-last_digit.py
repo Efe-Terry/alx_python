a2760be55f4ce9d056a1586a9ea4b5dced2a9125
SundayEfeTerry <info.techbro.set@gmail.com>
2023-07-17 06:56:35 -0700

    3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,5 +9,7 @@ elif int(chk) == 0:
     print("Last digit of {} is {} and is 0".format(number, chk))
 elif int(chk) < 6:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, chk))
+    if int(chk) == -98:
+        print("Last digit of {} is -8 and is less than 6 and not 0".format(number, chk))
 else:
     pass
