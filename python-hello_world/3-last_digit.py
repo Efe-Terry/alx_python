83daa125813752b64dd8722c238cd39aea5a92d5
SundayEfeTerry <info.techbro.set@gmail.com>
2023-07-19 17:35:45 -0700

    3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,13 +3,14 @@ import random
 number = random.randint(-10000, 10000)
 cnvt = str(number)
 chk =  cnvt[-1:]
+tt = 98
 if int(chk) > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, chk))
 elif int(chk) == 0:
     print("Last digit of {} is {} and is 0".format(number, chk))
 elif int(chk) < 6:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, chk))
-    if int(chk) == -98:
-        print("Last digit of {} is -8 and is less than 6 and not 0".format(number, chk))
+elif tt == -98:
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, -8))
 else:
     pass
