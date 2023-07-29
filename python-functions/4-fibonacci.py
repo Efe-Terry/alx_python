008726ecd75e51e9e33e481cd9c14190538a86e5
SundayEfeTerry <info.techbro.set@gmail.com>
2023-07-29 09:41:32 -0700

    4-fibonacci.py

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fibonacci_sequence(n):
+    
+    if n <= 0:
+        return []
+
+    sequence = [0, 1]
+    
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+
+    return sequence
