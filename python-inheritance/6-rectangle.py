a46a5114a70f8f4cee657bd7e80c88f66f47b1d8
SundayEfeTerry <info.techbro.set@gmail.com>
2023-08-03 16:22:58 -0700

    inherit from prev class

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""BaseGeometry class imported"""
+"""@BaseGeometry class imported"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 """Defines a class Rectangle that inherits from BaseGeometry."""
