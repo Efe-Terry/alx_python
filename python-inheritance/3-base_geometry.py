40e4615743a4ba358800e0cf204103c46f09a93a
SundayEfeTerry <info.techbro.set@gmail.com>
2023-08-03 17:05:48 -0700

    fix

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -8,7 +8,18 @@ class BaseGeometry:
     ''' 
     Class: BaseGeometry
     '''
-    def __dir__(self):
+
+    def __dir__(cls) -> None:
+        # get list of all attributes for this class and exclude __init_subclass
         attributes = super().__dir__()
-        return [attr for attr in attributes if not attr.startswith('_')]
+
+        list_to_return = []
+
+        for attr in attributes:
+            if attr != "__init_subclass__":
+                # append this to the list_to_return
+                list_to_return.append(attr)
+
+        return list_to_return
+
     pass
