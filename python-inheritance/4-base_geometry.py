0907d93a5d341ad2cb65e56bb337827346f3cdc4
SundayEfeTerry <info.techbro.set@gmail.com>
2023-08-11 04:13:16 -0700

    fix

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -21,6 +21,14 @@ class BaseGeometry(metaclass=BaseMetaClass):
 
     """Represent base geometry."""
 
+    def __dir__(cls):
+
+        '''
+        This is to exclude an attribute from printing
+        '''
+
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
     def area(self):
 
         """Not implemented."""
