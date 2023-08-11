4592cd371dd21a4c9244426a0571ec68c2e484a3
SundayEfeTerry <info.techbro.set@gmail.com>
2023-08-11 03:49:00 -0700

    new fix

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -5,7 +5,17 @@ This is an empty BaseGeometry class.
 """
 
 class BaseMetaClass(type):
+    
+    '''
+    Class: 
+    '''
+    
     def __dir__(cls):
+        
+        '''
+        This is to exclude an attribute from printing
+        '''
+
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
 class BaseGeometry(metaclass=BaseMetaClass):
@@ -15,6 +25,7 @@ class BaseGeometry(metaclass=BaseMetaClass):
     '''
 
     def __dir__(cls):
+        
         '''
         This is to exclude an attribute from printing
         '''
