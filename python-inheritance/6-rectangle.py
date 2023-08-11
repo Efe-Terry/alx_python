b76cb14cf081c08b5b069803d878480b442d1d61
SundayEfeTerry <info.techbro.set@gmail.com>
2023-08-11 04:21:04 -0700

    fix

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -11,6 +11,14 @@ class Rectangle(BaseGeometry):
 
     """Represent a rectangle using BaseGeometry."""
 
+    def __dir__(cls):
+
+        '''
+        This is to exclude an attribute from printing
+        '''
+
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
     def __init__(self, width, height):
 
         """Intialize a new Rectangle.
