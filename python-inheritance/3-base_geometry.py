d75501297b38794e90f50e32bbe15edf3cae6177
SundayEfeTerry <info.techbro.set@gmail.com>
2023-08-11 00:26:20 -0700

    self

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
+
 """
 This is an empty BaseGeometry class.
 """
 
+class BaseGeometry:
 
-class NoInitSubclassMeta(type):
-    def __init__(cls, name, bases, attrs):
-        if '__init_subclass__' in attrs:
-            del attrs['__init_subclass__']
-        super().__init__(name, bases, attrs)
-
-class BaseGeometry(metaclass=NoInitSubclassMeta):
     ''' 
     Class: BaseGeometry
     '''
 
-    pass
+    def __dir__(self):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
