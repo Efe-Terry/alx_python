fd8ad9cb5d5a424c03f9d2e0be1da0fd4ce14b53
SundayEfeTerry <info.techbro.set@gmail.com>
2023-08-11 03:15:19 -0700

    subclass

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -14,5 +14,5 @@ class BaseGeometry:
         '''
         This is to exclude an attribute from printing
         '''
-        return [attribute for attribute in super().__dir__() if attribute != '__init__']
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
