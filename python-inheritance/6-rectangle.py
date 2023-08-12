16068f21199403d2714d7723e0068c2f47d1e888
SundayEfeTerry <info.techbro.set@gmail.com>
2023-08-12 08:03:23 -0700

    fix

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -4,9 +4,14 @@
 
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
+class BaseGeometry(type):
+
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
 """Defines a class Rectangle that inherits from BaseGeometry."""
 
-class Rectangle(BaseGeometry):
+class Rectangle(metaclass=BaseGeometry):
 
     """Represent a rectangle using BaseGeometry."""
 
@@ -23,3 +28,6 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__height = height
 
+
+        def __dir__(cls):
+            return [attribute for attribute in super().__dir__() if attribute != '__init__']
